78a4be75a582dcf647283109af8df268ce469486
anggawanridho <55708748+anggawanridho@users.noreply.github.com>
2020-10-21 10:52:18 +0700

    Add files via upload
    first commit

---
+++ src/latihan6kambingstatic/Mamalia.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan6kambingstatic;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+public class Mamalia {
+    //Variable jumlahKambing dideklarasi kan sebagai statik
+    public static int jumlahKambing;
+}
